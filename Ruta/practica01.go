4612ca15788f1ac7b9a9805b0b4c95acd833fadb
Kevin Herrera <kevin.2110hc@gmail.com>
2024-05-16 16:48:14 -0500

    "Holi"

---
+++ Ruta/practica01.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func practica01 /*main*/() {
+
+	// https://go.dev/doc/
+
+	for i := 10; i <= 55; i++ {
+
+		if (i%2 == 0 && i != 16 && i%3 != 0) {
+			fmt.Println(i)
+		}
+	}
+	
+}
