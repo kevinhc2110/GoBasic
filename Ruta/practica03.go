cca2245c6f7cff3b3396e01e12c017ea8e03a3ec
Kevin Herrera <kevin.2110hc@gmail.com>
2024-05-17 10:48:42 -0500

    "Practica03"

--- Ruta/practica03.go
+++ Ruta/practica03.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
+var contactos = map[string]string {"Romeo":"3179812397", "Sofia": "3207783344"}
+
+var nombre string
+var numero string
+
 func main() {
 
+	var option int
+	fmt.Println("Ingrese una opción:")
+	fmt.Scanln(&option)
+
+	switch option {
+	case 1:
+		insertarContacto()	
+	}
+
+	fmt.Println(contactos)
+	
 }
+
+func insertarContacto() {
+
+	fmt.Println("Ingrese el numero")
+	fmt.Scanln(&numero)
+
+	fmt.Println("Ingrese el nombre:")
+	fmt.Scanln(&nombre)
+
+	contactos[nombre] = numero;
+}
+
